4056ccda00191cda8a7cb6fd965635e4503ee868
Chanaru Sampath <thegame@Chanarus-MacBook-Pro.local>
2025-01-06 09:37:05 +0530

    Initial commit

---
+++ prettier.config.mjs
@@ -0,0 +1,23 @@
+/**
+ * @type {import('prettier').Config}
+ */
+const config = {
+  semi: false,
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  importOrder: [
+    '^react(.*)$',
+    '^next(.*)$',
+    '<THIRD_PARTY_MODULES>',
+    '^@core/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+}
+
+export default config
